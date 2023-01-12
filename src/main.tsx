cede6fd1d6f333c54baa413c70e87ba0cd0ce005
hagaitsuki <haga-itsuki1@dmm.com>
2023-01-12 18:46:02 +0900

    install button

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import ReactDOM from 'react-dom/client'
 import { useWebAppManifest } from './useWebAppManifest'
 import manifest from "./manifest.webmanifest";
@@ -13,8 +13,16 @@ const App = () => {
       });
     }
   },[])
+  const [installPropmpt,setInstallPropmpt] = useState<any | null>(null)
+  useEffect(() => {
+    window.addEventListener("beforeinstallprompt",(e) => {
+      e.preventDefault;
+      setInstallPropmpt(e);
+    })
+  },[])
   return <div>
     PWA Sample1
+    {installPropmpt && <button onClick={() => installPropmpt.prompt()}>install</button>}
   </div>
 }
 
